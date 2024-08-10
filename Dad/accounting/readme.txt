21306cdb12768d3a229c8a409b84451a41f75d17
Andrew Eldredge <andrew.eldredge@3deolidar.com>
2024-08-10 17:06:31 -0600

    Accountind Dev Convenience

---
+++ Dad/accounting/readme.txt
@@ -0,0 +1,5 @@
+Work Tracked here
+
+https://docs.google.com/document/d/13zGS86reAnkON_lPxOhJc_ewIknfcXOAZ9lC-lMZGCA/
+
+
---
+++ Dad/accounting/runClassifier.bat
@@ -0,0 +1,1 @@
+python classifier.py "G:\My Drive\Household\PersonalFinanceOps\statements\2023\Chase5920_Activity20230101_20231231_20240525.CSV"
