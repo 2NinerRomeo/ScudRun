ed1084110441ba3d353f0e87196c1341ab062a05
Andrew Eldredge <andrew.eldredge@3deolidar.com>
2024-09-07 16:07:31 -0600

    Iterating transactions, printing a menu of categories

--- Dad/accounting/transactionCats.py
+++ Dad/accounting/transactionCats.py
@@ -1,18 +1,69 @@
 #import sys
 import dbConnect as db
 import pdb
+from os import system, name
+from time import sleep
 
 CREDFILENAME = 'creds.json'
 
+# define our clear function
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+def printLabelMenu(labelDict):
+    for cat in labelDict:
+        print(str(cat) + ". " + labelDict[cat])
+
+def printTransactionInfo(trans):
+    print(trans[1])
+    print(trans[2])
+    print(trans[3])
+    print(trans)
+
 def assignTransactions(db):
     theCursor = db.db.cursor()
-    query = "SELECT postDate,description,cardTransactions.amount,transCat.amount from cardTransactions LEFT JOIN transCat ON cardTransactions.id = transCat.transId;"
 
+    #get the list of categories, store in dictionary
+    query = "SELECT id,name FROM expCategories;"
+    theCursor.execute(query)
+    theResults = theCursor.fetchall()
+    catDict = {};
+    for row in theResults:
+        catDict[row[0]]=row[1]
+    clear()
+
+
+    #get the list of unmatched transactions
+    query = ("SELECT postDate,description,cardTransactions.amount,"
+            "transCat.amount from cardTransactions LEFT JOIN transCat"
+            " ON cardTransactions.id = transCat.transId;")
     theCursor.execute(query)
     theResults = theCursor.fetchall()
 
+    #how many reusults
+    transCount = len(theResults)
+    currentTrans = 1
+
+    #Have user classify single-category transactions
     for row in theResults:
-        print(row)
+        printLabelMenu(catDict)
+        print('\ntransaction ' + str(currentTrans) + '/' + str(transCount))
+        printTransactionInfo(row)
+        input('Enter Category:')
+        ##More here
+        # Quit, Skip
+        print('500ms')
+        sleep(0.5)
+        currentTrans = currentTrans + 1 
+        clear();
+        
+        
 
 
 
